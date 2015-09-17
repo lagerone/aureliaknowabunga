4480be09f1d48c0015a86ea648fe3a84b6bb8135
Daniel Lager <daniel.lager@tretton37.com>
2015-09-17 21:09:51 +0200

    Gets Hello world app with aurelia working

---
+++ Src/AureliaKnowabunga.Web/AureliaApp/app.js
@@ -0,0 +1,13 @@
+﻿export class App {
+    constructor () {
+        this.message = "";
+    }
+
+    activate () {
+        this.message = "Hello";
+    }
+
+    changeMessage () {
+        this.message = "Goodbye";
+    }
+}
